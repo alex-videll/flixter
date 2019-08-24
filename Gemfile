4a7c358844f34c3f45578a14b084739b27f2196b
Alexandra Videll <alexandra.videll@gmail.com>
2019-08-24 14:50:12 +0000

    most recent

--- Gemfile
+++ Gemfile
@@ -70,7 +70,8 @@ end
 
 gem 'simple_form'
 gem 'devise'
-gem 'carrierwave', '>= 2.0.0.rc', '< 3.0'
+gem 'carrierwave', '~> 1.3.1'
 gem "figaro"
 gem "fog-aws"
 gem "mini_magick"
+gem 'carrierwave-aws', '~> 1.0.2'
--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -3,6 +3,7 @@ class Instructor::SectionsController < ApplicationController
 	before_action :require_authorized_for_current_course
 
 	def new
+		@course = Course.find(params[:course_id])
 		@section = Section.new
 	end
 
@@ -21,7 +22,7 @@ class Instructor::SectionsController < ApplicationController
 
 	helper_method :current_course
 	def current_course
-		@current_course ||= Course.find(params[course_id])
+		current_course ||= Course.find(params[:course_id])
 	end
 
 	def section_params
