8f94ed84064fc0fcc213a311f63e05cf5b77b4a7
Alexandra Videll <alexandra.videll@gmail.com>
2019-08-26 23:58:27 +0000

    permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,28 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+  
+
 	def show
+		@lesson = current_lesson.section.course
 	end
 
 	private
 
-	helper_method :current_lesson
-	def current_lesson
-		@current_lesson ||= Lesson.find(params[:id])
-	end
+	def require_authorized_for_current_lesson
+    	if current_user.enrolled_in?(course)
+    		return true
+        else 
+        	redirect_to course_path, alert: 'Please enroll in this course to view lessons'
+    	end
+  	end
+
+	helper_method :current_section
+	def current_section
+    	@current_section ||= Section.find(params[:section_id])
+  	end
+
+	def lesson_params
+    	params.require(:lesson).permit(:title, :subtitle, :video)
+  	end
 end
