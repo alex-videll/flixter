9d30de6b158634eecc61ef83691ee9fe533378ad
Alexandra Videll <alexandra.videll@gmail.com>
2019-09-07 16:14:53 +0000

    next lesson button

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,23 +4,18 @@ class LessonsController < ApplicationController
   
 
 	def show
-		@lesson = current_lesson.section.course(lesson_params)
-    redirect_to lesson_path(lesson)
 	end
 
 	private
 
 	def require_authorized_for_current_lesson
-    current_user.enrolled_in?(course_url.to_s)
-    redirect_to course_path
+     if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You must be enrolled to view this lesson."
+    end
   end
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-
-	def lesson_params
-    	params.require(:lesson).permit(:title, :subtitle, :video)
-  	end
 end
