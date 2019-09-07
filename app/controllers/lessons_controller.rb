9050069ba88199a31522fa403fd9986503e62725
Alexandra Videll <alexandra.videll@gmail.com>
2019-09-07 15:09:04 +0000

    more lessons controller edits

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,32 +4,21 @@ class LessonsController < ApplicationController
   
 
 	def show
-		@lesson = current_section.lessons(lesson_params)
+		@lesson = current_lesson.section.course(lesson_params)
     redirect_to lesson_path(lesson)
 	end
 
 	private
 
 	def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user
-      redirect_to root_url, alert: 'Please enroll in this course to view lessons'
-    end
+    current_user.enrolled_in?(course_url.to_s)
+    redirect_to course_path
   end
 
+  helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-
-  def require_authorized_for_current_section
-    if current_section.course.user != current_user
-      redirect_to root_url, alert: 'Please enroll in this course to view sections'
-    end
-  end
-
-	helper_method :current_section
-	def current_section
-    	@current_section ||= Section.find(params[:section_id])
-  	end
 
 	def lesson_params
     	params.require(:lesson).permit(:title, :subtitle, :video)
